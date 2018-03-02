376beeac74b9f810bd599b9dcd2d4e1394ca4e80
Matt Shoemaker <shoemaker.277@osu.edu>
2018-03-02 16:05:09 -0500

    Implemented Vertex constructors, instance variables, and getters/setters

--- src/vertex/Vertex.java
+++ src/vertex/Vertex.java
@@ -6,9 +6,91 @@ public class Vertex {
     private Vertex parent;
     private int dist;
     private Color color;
+    private int discovery;
+    private int finish;
 
-    public enum Color {
-        WHITE, GRAY, BLACK, RED, YELLOW
+    public static enum Color {
+        WHITE, GRAY, BLACK, RED, YELLOW, NONE
+    }
+
+    public Vertex(int id) {
+        this.id = id;
+        this.parent = null;
+        this.dist = -1;
+        this.color = Color.NONE;
+        this.discovery = -1;
+        this.finish = -1;
+    }
+
+    public Vertex(int id, Vertex parent) {
+        this.id = id;
+        this.parent = parent;
+        this.dist = -1;
+        this.color = Color.NONE;
+        this.discovery = -1;
+        this.finish = -1;
+    }
+
+    public Vertex(int id, Vertex parent, int dist) {
+        this.id = id;
+        this.parent = parent;
+        this.dist = dist;
+        this.color = Color.NONE;
+        this.discovery = -1;
+        this.finish = -1;
+    }
+
+    public Vertex(int id, Vertex parent, int dist, Color color) {
+        this.id = id;
+        this.parent = parent;
+        this.dist = dist;
+        this.color = color;
+        this.discovery = -1;
+        this.finish = -1;
+    }
+
+    public int id() {
+        return this.id;
+    }
+
+    public Vertex parent() {
+        return this.parent;
+    }
+
+    public int dist() {
+        return this.dist;
+    }
+
+    public Color color() {
+        return this.color;
+    }
+
+    public int discovery() {
+        return this.discovery;
+    }
+
+    public int finish() {
+        return this.finish;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setDiscovery(int discovery) {
+        this.discovery = discovery;
+    }
+
+    public void setFinish(int finish) {
+        this.finish = finish;
     }
 
 }
